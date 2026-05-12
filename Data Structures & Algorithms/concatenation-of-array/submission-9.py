97d9f4275a26c0ad4e5c77c5f9676f5a25efcd4e
Renish Aryan <renisharyan2000@gmail.com>
2026-05-13 00:51:27 +0530

    Add: concatenation-of-array - submission-9

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-9.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums):
+        conc = []
+        for i in range(len(nums)):
+            conc.append(nums[i])
+
+        if len(nums) == len(conc):
+            for i in range(len(nums)):
+                conc.append(nums[i])
+        
+        return conc
