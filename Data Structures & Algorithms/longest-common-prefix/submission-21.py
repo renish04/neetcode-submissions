a514e5a4e9df4631a4ac758683add77b6c992cfa
Renish Aryan <renisharyan2000@gmail.com>
2026-05-13 17:40:16 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-21.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        shortword = strs[0]
+        for i in range(len(strs)):
+            if len(strs[i]) < len(shortword):
+                shortword = strs[i]
+            if strs[i]  == "":
+                return ""
+        
+        for i in range(len(shortword)):
+            x = shortword[0:i+1]
+            for j in range(len(strs)):
+                if shortword[0] != strs[j][0]:
+                    return ""
+                if x == strs[j][0:i+1]:
+                    pass
+                elif x != strs[j][0:i+1]:
+                    print(f"exectuted for x : {x}")
+                    return shortword[0:i]
+                    
+                
