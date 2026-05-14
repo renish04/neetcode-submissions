e6bc6b48f9ea1d1a46419089976ae066b2dcbc8a
Renish Aryan <renisharyan2000@gmail.com>
2026-05-14 21:01:20 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = nums[0]
+        count = 0
+
+        for i in range(len(nums)):
+            if nums[i] == candidate:
+                count += 1
+
+            if nums[i] != candidate:
+                count -= 1
+                if count == 0:
+                    candidate = nums[i]
+
+        return candidate
