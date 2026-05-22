1d2256f5fa3224421241cb695c46b45cef286b0f
Renish Aryan <renisharyan2000@gmail.com>
2026-05-22 23:12:30 +0530

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        pre_sum = 0
+        for i in range(len(nums)):
+            nums[i] += pre_sum
+            pre_sum = nums[i]
+
+        hash_nums = {}
+        total_sub_arrays = 0
+
+        for i in range(len(nums)):
+            if nums[i] == k:
+                total_sub_arrays += 1
+                if nums[i] in hash_nums:
+                    hash_nums[nums[i]] += 1
+                else:
+                    hash_nums[nums[i]] = 1
+            elif nums[i] != k and nums[i] - k in hash_nums:
+                total_sub_arrays += hash_nums[nums[i]-k]
+                if nums[i] in hash_nums:
+                    hash_nums[nums[i]] += 1
+                else:
+                    hash_nums[nums[i]] = 1
+            else:
+                if nums[i] in hash_nums:
+                    hash_nums[nums[i]] += 1
+                else:
+                    hash_nums[nums[i]] = 1
+
+        return total_sub_arrays 
