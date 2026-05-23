4a6ef79428fd80c70cf64ef81c55c6ec709f5010
Renish Aryan <renisharyan2000@gmail.com>
2026-05-23 23:13:34 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,78 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for i in range(len(board)):
+            check_set = set()
+            for j in range(i):
+                if board[i][j] != ".":
+                    if board[i][j] in check_set:
+                        print("1")
+                        return False
+                    else:
+                        check_set.add(board[i][j])
+        
+        for i in range(len(board)):
+            check_set = set()
+            for j in range(i):
+                if board[j][i] != ".":
+                    if board[j][i] in check_set:
+                        print("2")
+                        return False
+                    else:
+                        check_set.add(board[j][i])
+        i = 0
+        while i < len(board):
+            
+            j = 0
+            while j < len(board):
+                check_set = set()
+                if board[i][j] in check_set:
+                    print("3")
+                    return False
+                elif board[i][j] not in check_set and board[i][j] != ".":
+                    check_set.add(board[i][j])
+                if board[i][j+1] in check_set:
+                    print("4")
+                    return False
+                elif board[i][j+1] not in check_set and board[i][j+1] != ".":
+                    check_set.add(board[i][j+1])
+                if board[i][j+2] in check_set:
+                    print("5")
+                    return False
+                elif board[i][j+2] not in check_set and board[i][j+2] != ".":
+                    check_set.add(board[i][j+2])
+                if board[i+1][j] in check_set:
+                    print("6")
+                    return False
+                elif board[i+1][j] not in check_set and board[i+1][j] != ".":
+                    check_set.add(board[i+1][j])
+                if board[i+1][j+1] in check_set:
+                    print("7", i, j)
+                    return False
+                elif board[i+1][j+1] not in check_set and board[i+1][j+1] != ".":
+                    check_set.add(board[i+1][j+1])
+                if board[i+1][j+2] in check_set:
+                    print("8")
+                    return False
+                elif board[i+1][j+2] not in check_set and board[i+1][j+2] != ".":
+                    check_set.add(board[i+1][j+2])
+                if board[i+2][j] in check_set:
+                    print("9")
+                    return False
+                elif board[i+2][j] not in check_set and board[i+2][j] != ".":
+                    check_set.add(board[i+2][j])
+                if board[i+2][j+1] in check_set:
+                    print("10")
+                    return False
+                elif board[i+2][j+1] not in check_set and board[i+2][j+1] != ".":
+                    check_set.add(board[i+2][j+1])
+                print(check_set)
+                if board[i+2][j+2] in check_set:
+                    print("11", i, j)
+                    return False
+                elif board[i+2][j+2] not in check_set and board[i+2][j+2] != ".":
+                    check_set.add(board[i+2][j+2])
+                j += 3
+            i += 3
+        
+        return True
