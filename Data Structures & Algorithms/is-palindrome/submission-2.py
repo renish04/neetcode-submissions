5ca09bb24313bbe6170035780a6eb3659b8d12d0
Renish Aryan <renisharyan2000@gmail.com>
2026-05-23 13:29:21 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(s.split())
+        s = s.lower()
+        print(s)
+        n = len(s)
+
+        left = 0
+        right = n-1
+
+        while left < right:
+            if not (48 <= ord(s[left]) <= 57 or  65 <= ord(s[left]) <= 90 or 97 <= ord(s[left]) <= 122):
+                left += 1
+            if not (48 <= ord(s[right]) <= 57 or  65 <= ord(s[right]) <= 90 or 97 <= ord(s[right]) <= 122):
+                right -= 1
+            elif s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
