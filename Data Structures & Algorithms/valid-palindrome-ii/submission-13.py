737386342007547b7c3b47260fac952d7521a25c
Renish Aryan <renisharyan2000@gmail.com>
2026-05-23 15:43:40 +0530

    Add: valid-palindrome-ii - submission-13

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-13.py
@@ -0,0 +1,45 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        n = len(s)
+
+        left = 0
+        right = n - 1
+        count = 1
+        while left < right:
+            if s[left] == s[right]:
+                if left == right:
+                    return True
+                else:
+                    left += 1
+                    right -= 1
+            else:
+                if count != 0:
+                    if s[left+1] == s[right]:
+                        if left+1 >= right:
+                            left += 2
+                            right -= 1
+                            count = 0
+                        else:
+                            if s[left+2] == s[right-1]:
+                                left += 2
+                                right -= 1
+                                count = 0
+                            else:
+                                return False 
+                    elif s[left] == s[right-1]:
+                        if right-1 <= left: 
+                            left += 1
+                            right -= 2
+                            count = 0
+                        else:
+                            if s[left + 1] == s[right-2]:
+                                left += 1
+                                right -= 2
+                                count = 0
+                            else:
+                                return False
+                    else:
+                        return False
+                else:
+                    return False
+        return True
