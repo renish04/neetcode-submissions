9629fcf4e27a80e32dca7e437ef9f8723a217c2b
Renish Aryan <renisharyan2000@gmail.com>
2026-05-24 00:57:16 +0530

    Add: first-missing-positive - submission-6

---
+++ Data Structures & Algorithms/first-missing-positive/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[i] <=0 or nums[i] > len(nums):
+                nums[i] = ""
+            else:
+                k = nums[i]
+                if nums[k-1] == k and i != k-1:
+                    nums[i] = ""
+                else:
+                    nums[i], nums[k-1] = nums[k-1], nums[i]
+        print(nums)    
+        for i in range(len(nums)):
+            if nums[i] == "":
+                return i+1
+            else:
+                continue
+                
+        return len(nums)+1
