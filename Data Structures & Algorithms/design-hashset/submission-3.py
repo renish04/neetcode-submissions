819a4bf43f55be002a0df1eade16dea63d2a80e5
Renish Aryan <renisharyan2000@gmail.com>
2026-05-25 12:58:48 +0530

    Add: design-hashset - submission-3

---
+++ Data Structures & Algorithms/design-hashset/submission-3.py
@@ -0,0 +1,24 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.con = [False] * 1000001
+
+    def add(self, key: int) -> None:
+        if self.con[key] == False:
+            self.con[key] = True
+
+    def remove(self, key: int) -> None:
+        self.con[key] = False
+
+    def contains(self, key: int) -> bool:
+        if self.con[key] != False:
+            return True
+        else:
+            return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
