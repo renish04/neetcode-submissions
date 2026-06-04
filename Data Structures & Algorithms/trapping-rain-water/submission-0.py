b86703dff4bd92c748bf714ff11f9627f72187e0
Renish Aryan <renisharyan2000@gmail.com>
2026-06-04 23:32:59 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        area = 0
+        dict = {}
+
+        for i in range(len(height)):
+            dict[i] = height[i]
+
+        i = 0
+        while i < len(height) - 2:
+            dict.pop(i)
+            j = i+1
+            if not height[j] < height[i]:
+                i += 1
+            else:
+                dict.pop(j)
+                max_key = max(dict, key=dict.get)
+
+                if height[max_key] > height[j]:
+                    k = j + 1
+                    counter = height[j]
+                    
+                    while height[k] <= height[j]:
+                        counter += height[k]
+                        dict.pop(k)
+                        k += 1
+                    if height[k] >= height[i]:
+                        area += ((k-j)*height[i])-counter
+                        i = k
+                    else:
+                        area += ((k-j)*height[k])-counter
+                        i = k
+                else:
+                    i += 1
+        return area
+
+
