9051a7032cb0382871b237aa354fe02f259bdc69
Renish Aryan <renisharyan2000@gmail.com>
2026-06-09 21:12:30 +0530

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.py
@@ -0,0 +1,31 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        if len(position) == 1:
+            return 1
+
+        hashmap = {}
+
+        for i in range(len(position)):
+            hashmap[position[i]] = speed[i]
+        
+        position.sort()
+
+        if (target - position[-1]) % hashmap[position[-1]] == 0:
+            factor = (target - position[-1]) // hashmap[position[-1]]
+        else:
+            factor = ((target - position[-1]) // hashmap[position[-1]]) + 1
+
+        for i in range(len(position)):
+            position[i] += (factor*hashmap[position[i]])
+        
+        fleet = 0
+
+        while position:
+            r = position.pop()
+            while position and position[-1] >= r or position[-1] > target:
+                position.pop()
+            fleet += 1
+
+        
+        return fleet
+
