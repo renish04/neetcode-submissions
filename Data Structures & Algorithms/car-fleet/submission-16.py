7d651b8916cebe8368cc4705147904be2ba017c5
Renish Aryan <renisharyan2000@gmail.com>
2026-06-11 00:36:05 +0530

    Add: car-fleet - submission-16

---
+++ Data Structures & Algorithms/car-fleet/submission-16.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        time = {}
+
+        for i in range(len(position)):
+            time[position[i]] = (target - position[i]) / speed[i]
+
+        position.sort()
+
+        fleet = 0
+
+        for i in range(len(position)):
+            fleet += 1    
+            r = position.pop()
+            t = time[r]
+
+            while position and time[position[-1]] <= t:
+                position.pop()
+                continue
+            if not position:
+                break
+        return fleet
