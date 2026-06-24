327dc896c40af82b7fa6a723773436ce13c2b703
Renish Aryan <renisharyan2000@gmail.com>
2026-06-24 14:04:08 +0530

    Add: binary-tree-preorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        nodes = []
+
+        def recurse(root):
+            if not root:
+                return
+            
+            nodes.append(root.val)
+            recurse(root.left)
+            recurse(root.right)
+
+        recurse(root)
+        return nodes
